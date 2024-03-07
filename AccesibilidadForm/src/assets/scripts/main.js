0c901f7e7afd9bb4c0353084545b348179f855e9
Lainezz <diegolinaresmojacar@gmail.com>
2024-03-07 13:32:44 +0100

    adicion de codigo para clase

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -3,6 +3,7 @@
 function onIniciar() {
     console.log("Hola Mundo");
 
+    // CODIGO DEL TAB
     // 1º es conseguir todas las pestañas aka los botones
     // 2º conseguir todos los bloques de texto
     const botonesTab = document.querySelectorAll(".botonesTab__pestana");
@@ -28,6 +29,25 @@ function onIniciar() {
         })
     })
 
+    // CÓDIGO DEL ACORDEON
+    // 1º Seleccionamos los nodos en cuestión. Los nodos que vamos a tratar
+    // h2Titulo va a tener un eventListener de click
+    const h2Titulo = document.querySelectorAll(".divContenedor__titulo")
+    // pContenido es al que vamos a modificarle las clases css... la classList
+    const pContenido = document.querySelectorAll(".divContenedor__contenido");
+
+    h2Titulo.forEach((nodo, i)=>{
+
+      nodo.addEventListener("click", ()=>{
+
+        // Si el pContenido tiene la clase css divContenedor__contenido--activo, la quitamos
+        if (pContenido[i].classList.contains("divContenedor__contenido--activo")) {
+          pContenido[i].classList.remove("divContenedor__contenido--activo");
+        } else { // y si no, la añadimos
+          pContenido[i].classList.add("divContenedor__contenido--activo");
+        }        
+      })
+    })
 }
 
 
