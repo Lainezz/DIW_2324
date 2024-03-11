93a0d3a9fe3b2383252da62c4ff6fb19ffa90d9f
Lainezz <diegolinaresmojacar@gmail.com>
2024-03-11 12:50:21 +0100

    adicion de carrousel

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -1,5 +1,4 @@
 
-
 function onIniciar() {
     console.log("Hola Mundo");
 
@@ -49,30 +48,33 @@ function onIniciar() {
       })
     })
 
-    // CÓDIGO DEL LIGHTBOX
-    // 1º Seleccionamos el nodo al que queremos añadir evento click
-    const imagenes = document.querySelectorAll(".div-img__img");
-    // Seleccionamos también el nodo al que cambiar la clase activo
-    const divLightbox = document.querySelector(".main__ligthbox-proyecto-selected");
-    const imgLigthbox = document.querySelector(".div-img__img--lightbox");
+    // CODIGO DEL CARROUSEL
+    // seleccionamos el nodo con clase carrousel__grande
+    const divCarrouselGrande = document.querySelector(".carrousel__grande");
+    // debemos añadir una funcionalidad click a los botones... tenemos que seleccionar TODOS los botones
+    const botones = document.querySelectorAll(".botones__boton");
 
-    imagenes.forEach((cadaImagen, i) => {
-      cadaImagen.addEventListener("click", () => {
-        // Podemos ver por pantalla que cadaImagen.src es el atributo src del nodo img... es decir... la ruta de la imagen
-        console.log(cadaImagen.src); // Cada nodo es cada imagen del lightbox, es decir, las imagenes en pequeño
+    // Una vez seleccionados los nodos, añadimos el eventListener a TODOS LOS BOTONES
+    botones.forEach((boton, i)=>{
 
-        // Añadimos la clase activo al divLightbox
-        divLightbox.classList.add("activo"); // divLightbox es el div con position fixed que se abre al hacer click en el nodo (divImg)
-        // Le ponemos, al atributo src de la img del lightbox (la grande) el src de la imagen a la que se le ha hecho click
-        imgLigthbox.setAttribute("src", cadaImagen.src);
-      });
+      boton.addEventListener("click", ()=>{
+        console.log(`Se ha pulsado el boton nº ${i} :)`)
+
+        // Quitamos la clase css activo a TODOS LOS BOTONES
+        botones.forEach((botonQuitar)=>{
+            botonQuitar.classList.remove("activo")
+        });
+        // para acto seguido, añadirsela al boton AL QUE SE LE HA HECHO CLICK
+        boton.classList.add("activo")
+
+        let calculoPx = i * -50;
+        console.log(`Click en ${i}: calculo= ${calculoPx}`)
+
+        divCarrouselGrande.style.transform = `translateX(${calculoPx}%)`;
+      })
+      
+
     });
-
-    // Ya sólo queda dale funcionalidad al boton cerrar
-    const botonCerrar = document.querySelector(".ligthbox-proyecto-selected__boton")
-    botonCerrar.addEventListener("click", ()=>{
-      divLightbox.classList.remove("activo");
-    })
 }
 
 
